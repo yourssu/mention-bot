a3c1c34cdb8f7c97df0524e646cb9c118d4af615
fecapark <fecapark@gmail.com>
2025-02-28 19:54:58 +0900

    :sparkles: env 타입 인텔리센스 추적 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BOT_USER_OAUTH_TOKEN: string;
+  readonly VITE_SLACK_APP_ID: string;
+  readonly VITE_SLACK_CLIENT_ID: string;
+  readonly VITE_SLACK_CLIENT_SECRET: string;
+  readonly VITE_SLACK_SIGNING_SECRET: string;
+  readonly VITE_SLACK_SOCKET_APP_TOKEN: string;
+  readonly VITE_SLACK_VERIFICATION_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
