43b71dded6336a32eeafb3ec33d2207ffa35f74d
fecapark <fecapark@gmail.com>
2025-03-05 17:52:39 +0900

    :wrench: vite-node watch 모드 추가

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,25 @@
-import { Member } from '@slack/web-api/dist/types/response/UsersListResponse';
-
-import { getAllSlackMembers } from '@/apis/member';
 import { slackApp } from '@/core/slack';
-import { NotionMemberPosition } from '@/types/member';
-import { findSlackMemberByNotionMember, getNotionMembersWithPosition } from '@/utils/member';
 
-const test = async () => {
-  NotionMemberPosition.map(async (position) => {
-    const notionMembers = await getNotionMembersWithPosition(position);
-    await getAllSlackMembers(); // for ensure slack members cached
-    const slackMembers = (
-      await Promise.all(
-        notionMembers.map((member) => {
-          return findSlackMemberByNotionMember(member);
-        })
-      )
-    ).filter(Boolean) as Member[];
-    console.log(position, notionMembers.length, slackMembers.length);
+const setSlackBot = async () => {
+  await slackApp.start(3000);
+  slackApp.logger.info('⚡️ 슬랙 봇이 켜졌어요.');
+};
+
+const setSlackAppReloader = () => {
+  if (!import.meta.hot) {
+    return;
+  }
+
+  import.meta.hot.on('vite:beforeFullReload', () => {
+    slackApp.stop();
+  });
+
+  import.meta.hot.dispose(() => {
+    slackApp.stop();
   });
 };
 
 (async () => {
-  await slackApp.start(3000);
-  slackApp.logger.info('⚡️ Bolt app is running!');
-
-  // await test();
+  await setSlackBot();
+  setSlackAppReloader();
 })();
